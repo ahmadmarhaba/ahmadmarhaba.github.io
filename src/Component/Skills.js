b82578f54c5f18e986c0f2dab6a3ac238f22894c
ahmadmarhaba <dragon.8881@hotmail.com>
2022-10-29 17:45:42 +0300

    updating skills

--- src/Component/Skills.js
+++ src/Component/Skills.js
@@ -6,23 +6,21 @@ function Skills() {
             <li><h2>Skills</h2></li>
             <li><h3>Front-end</h3></li>
             <li className='split points'>
-                <span>{`React js`}</span>
-                <span>{`Next js`}</span>
-                <span>{`Electron js`}</span>
-                <span>{`React Native`}</span>
-                <span>{`MVC .Net`}</span>
-                <span>{`Unity`}</span>
-                <span>{`Xamarin`}</span>
+                <span>{`Reactjs`}</span>
+                <span>{`Nextjs`}</span>
+                <span>{`.Net`}</span>
                 <span>{`Redux`}</span>
-                <span>{`JQuery`}</span>
                 <span>{`Ajax requests`}</span>
-                <span>{`Client visualization`}</span>
+                {/* <span>{`Electron js`}</span> */}
+                {/* <span>{`React Native`}</span> */}
+                {/* <span>{`Unity`}</span> */}
+                {/* <span>{`Xamarin`}</span> */}
+                {/* <span>{`JQuery`}</span> */}
+                {/* <span>{`Client visualization`}</span> */}
             </li>
             <li><h3>Back-end</h3></li>
             <li className='split points'>
                 <span>{`Node js`}</span>
-                <span>{`Asp .net`}</span>
-                <span>{`Php`}</span>
                 <span>{`Redis`}</span>
                 <span>{`Rest API`}</span>
                 <span>{`SQL Server`}</span>
@@ -30,47 +28,49 @@ function Skills() {
                 <span>{`PostgreSQL`}</span>
                 <span>{`Firebase`}</span>
                 <span>{`MongoDB`}</span>
-                <span>{`Mongoose`}</span>
-                <span>{`Prisma`}</span>
-                <span>{`Passportjs`}</span>
                 <span>{`Socket.io`}</span>
-                <span>{`WebRtc`}</span>
-                <span>{`SignalR`}</span>
-                <span>{`Cloudinary`}</span>
-                <span>{`Nginx`}</span>
-                <span>{`Apache`}</span>
+                {/* <span>{`Asp .net`}</span> */}
+                {/* <span>{`Php`}</span> */}
+                {/* <span>{`Mongoose`}</span> */}
+                {/* <span>{`Prisma`}</span> */}
+                {/* <span>{`Passportjs`}</span> */}
+                {/* <span>{`WebRtc`}</span> */}
+                {/* <span>{`SignalR`}</span> */}
+                {/* <span>{`Cloudinary`}</span> */}
+                {/* <span>{`Nginx`}</span> */}
+                {/* <span>{`Apache`}</span> */}
             </li>
             <li><h3>DevOps</h3></li>
             <li className='split points'>
                 <span>{`CI/CD`}</span>
-                <span>{`Docker`}</span>
-                <span>{`Terraform`}</span>
-                <span>{`Kubernetes`}</span>
                 <span>{`Github actions`}</span>
                 <span>{`Github pages`}</span>
-                <span>{`Vercel`}</span>
-                <span>{`Netlify`}</span>
-                <span>{`Heruko`}</span>
-                <span>{`Stripe`}</span>
-                <span>{`JWT`}</span>
-                <span>{`Web3`}</span>
+                {/* <span>{`Docker`}</span> */}
+                {/* <span>{`Terraform`}</span> */}
+                {/* <span>{`Kubernetes`}</span> */}
+                {/* <span>{`Vercel`}</span> */}
+                {/* <span>{`Netlify`}</span> */}
+                {/* <span>{`Heruko`}</span> */}
+                {/* <span>{`Stripe`}</span> */}
+                {/* <span>{`JWT`}</span> */}
+                {/* <span>{`Web3`}</span> */}
             </li>
             <li><h3>Other</h3></li>
             <li className='split points'>
                 <span>{`Git`}</span>
                 <span>{`Agile`}</span>
                 <span>{`Scrum`}</span>
-                <span>{`Object oriented programming`}</span>
-                <span>{`Server management`}</span>
-                <span>{`Hosting management`}</span>
+                <span>{`OOP`}</span>
                 <span>{`JSON`}</span>
                 <span>{`XML`}</span>
+                {/* <span>{`Server management`}</span> */}
+                {/* <span>{`Hosting management`}</span> */}
             </li>
             <li><h3>Languages</h3></li>
             <li className='split points'>
-                <span>{`Sql`}</span>
-                <span>{`Javascript`}</span>
                 <span>{`Typescript`}</span>
+                <span>{`Javascript`}</span>
+                <span>{`Sql`}</span>
                 <span>{`C#`}</span>
                 <span>{`Php`}</span>
                 <span>{`Java`}</span>
